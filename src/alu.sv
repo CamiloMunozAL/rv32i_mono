5af28ae3c5117f0ef23f9c6972f57fe03ae65a25
CamiloMunozAL <camilo.munoz2@utp.edu.co>
2025-11-20 15:15:39 -0500

    alu se agrega valor por defecto

--- src/alu.sv
+++ src/alu.sv
@@ -9,18 +9,18 @@ module alu(
 
   always_comb begin
     case (ALUOp)
-    4'b0000: ALURes = A + B; // Suma
-    4'b1000: ALURes = A - B; // Resta
-    4'b0001: ALURes = A << B; // Desplazamiento lógico a la izquierda
-    4'b0010: ALURes = A < B; // Menor que (set on less than)
-    4'b0011: ALURes = ($unsigned(A) < $unsigned(B)) ? 32'sd1 : 32'sd0; // Menor que sin signo
-    4'b0100: ALURes = A ^ B; // XOR
-    4'b0101: ALURes = A >> B; // Desplazamiento lógico a la derecha
-    4'b1101: ALURes = A >>> B; // Desplazamiento aritmético a la derecha
-    4'b0110: ALURes = A | B; // OR
-    4'b0111: ALURes = A & B; // AND
-    4'b1001: ALURes = B; // Pass B (para instrucciones como LUI)
-
+      4'b0000: ALURes = A + B; // Suma
+      4'b1000: ALURes = A - B; // Resta
+      4'b0001: ALURes = A << B[4:0]; // Desplazamiento lógico a la izquierda
+      4'b0010: ALURes = (A < B) ? 32'd1 : 32'd0; // Menor que (set on less than)
+      4'b0011: ALURes = ($unsigned(A) < $unsigned(B)) ? 32'd1 : 32'd0; // Menor que sin signo
+      4'b0100: ALURes = A ^ B; // XOR
+      4'b0101: ALURes = A >> B[4:0]; // Desplazamiento lógico a la derecha
+      4'b1101: ALURes = A >>> B[4:0]; // Desplazamiento aritmético a la derecha
+      4'b0110: ALURes = A | B; // OR
+      4'b0111: ALURes = A & B; // AND
+      4'b1001: ALURes = B; // Pass B (para instrucciones como LUI)
+      default: ALURes = 32'd0; // Valor por defecto
     endcase
   end
 

cdac75045821afc9a8dffefc48c31a8b2ef117fb
CamiloMunozAL <camilo.munoz2@utp.edu.co>
2025-11-20 15:19:24 -0500

    quita reset pc

--- src/pc.sv
+++ src/pc.sv
@@ -2,12 +2,12 @@
 
 module pc(
   input logic clk, // Reloj del sistema
-  input logic reset, // Señal de reinicio
+  input logic reset, // Señal de reinicio (activo alto)
   input logic [31:0] next_pc, // Dirección de la siguiente instrucción
   output logic [31:0] pc_out // Dirección actual del PC
 );
 
-  always_ff @(posedge clk or posedge reset) begin
+  always_ff @(posedge clk) begin
     if (reset) begin
       pc_out <= 32'b0; // Reiniciar el PC a 0
     end else begin
